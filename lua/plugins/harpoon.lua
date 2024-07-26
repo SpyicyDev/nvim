d45750d5d843e880c4d32c189b5ba67ad4145454
Mack Haymond <mack.haymond@icloud.com>
2024-07-26 12:29:07 -0400

    yuh

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
             vim.keymap.set("n", "<leader>hg", require("harpoon.mark").add_file)
             vim.keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
 
-            for i = 1, 8 do
+            for i = 1, 5 do
                 vim.keymap.set("n", "<leader>" .. i, function() require("harpoon.ui").nav_file(i) end)
             end
 
