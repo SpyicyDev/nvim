57d2592a98d821a2649732666fbbec0381445bdc
Mack Haymond <mack.haymond@icloud.com>
2026-01-27 21:09:31 -0800

    enable copilot-cmp integration

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,51 +2,28 @@ return {
     {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
-        cond = false,
         event = "InsertEnter",
         opts = {
-            suggestion = {
-                enabled = true,
-                auto_trigger = true,
-                keymap = {
-                    next = "<M-]>",
-                    prev = "<M-[>",
-                    accept = false,
-                },
-            },
+            suggestion = { enabled = false },
+            panel = { enabled = false },
         },
-        init = function()
-                vim.keymap.set("i", '<Tab>', function()
-                    if require("copilot.suggestion").is_visible() then
-                        require("copilot.suggestion").accept()
-                    else
-                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
-                    end
-                end, { silent = true })
-
-                -- Mapping for Shift-Tab to accept the current line from suggestions
-                vim.keymap.set("i", '<S-Tab>', function()
-                    if require("copilot.suggestion").is_visible() then
-                        require("copilot.suggestion").accept_line()
-                    else
-                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", false)
-                    end
-                end, { silent = true })
-            end
     },
 
     {
         "zbirenbaum/copilot-cmp",
-        cond = false,
-        opts = {},
+        dependencies = { "zbirenbaum/copilot.lua" },
+        config = function()
+            require("copilot_cmp").setup()
+        end,
     },
 
     {
         "gptlang/CopilotChat.nvim",
+        cond = false,
     },
 
     {
-        'AndreM222/copilot-lualine',
-        cond = false,
-    }
+        "AndreM222/copilot-lualine",
+        dependencies = { "zbirenbaum/copilot.lua" },
+    },
 }
